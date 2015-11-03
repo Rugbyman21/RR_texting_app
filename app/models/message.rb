cf34ba77efdba880609816e6467588cdc76be9c2
Trevor Scherling <scherling.trevor@gmail.com>
2015-11-03 10:09:15 -0800

    fix setup

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,13 +7,14 @@ private
     begin
       response = RestClient::Request.new(
         :method => :post,
-        :url =>'https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json',
+        :url =>"https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
         :user => ENV['TWILIO_ACCOUNT_SID'],
         :password => ENV['TWILIO_AUTH_TOKEN'],
         :payload => {:Body => body,
                        :From => from,
-                       :To => to}
-      ).execute
+                       :To => to }
+        ).execute
+
 
 
     rescue RestClient::BadRequest => error
